2d96e86a70e06e651b4506f0a78a2d442d9737aa
javier-rosas <jr2dj@virginia.edu>
2023-08-18 15:22:17 -0500

    added include-dependencies plugin, added api routes

--- src/mongo/handler.ts
+++ src/mongo/handler.ts
@@ -1,7 +1,11 @@
 import middy from "middy";
 import { mongooseConnect } from "../common/mongoose/mongooseConnect";
 import { createOrUpdateUser } from "../common/mongoose/queries/user";
-import { getFilesByUserEmail } from "../common/mongoose/queries/file";
+import {
+  getFilesByUserEmail,
+  getFileByUserEmailAndFilename,
+  deleteFileByUserEmailAndFilename,
+} from "../common/mongoose/queries/file";
 import { createResponse } from "../common/utils/createResponse";
 import { verifyTokenMiddleware } from "../common/utils/verifyTokenMiddleware";
 
@@ -19,15 +23,32 @@ async function handleGetFilesByUserEmail(event: any) {
   return await getFilesByUserEmail(userEmail);
 }
 
+// Handle GET request to fetch file by user email and filename
+async function handleGetFileByUserEmailAndFilename(event: any) {
+  const userEmail = event.pathParameters.userEmail;
+  const filename = event.pathParameters.filename;
+  return await getFileByUserEmailAndFilename(userEmail, filename);
+}
+
+// Handle DELETE request to delete file by user email and filename
+async function handleDeleteFileByUserEmailAndFilename(event: any) {
+  const userEmail = event.pathParameters.userEmail;
+  const filename = event.pathParameters.filename;
+  return await deleteFileByUserEmailAndFilename(userEmail, filename);
+}
+
 const mainHandler = async (event: any): Promise<any> => {
   try {
     await mongooseConnect();
-
     switch (event.routeKey) {
-      case "POST /mongo":
+      case "POST /users":
         return await handleCreateOrUpdateUser(event);
-      case "GET /mongo/{userEmail}/files":
+      case "GET /users/{userEmail}/files":
         return await handleGetFilesByUserEmail(event);
+      case "GET /users/{userEmail}/files/{filename}":
+        return await handleGetFileByUserEmailAndFilename(event);
+      case "DELETE /users/{userEmail}/files/{filename}":
+        return await handleDeleteFileByUserEmailAndFilename(event);
       default:
         return createResponse(404, { error: "Not Found" });
     }
